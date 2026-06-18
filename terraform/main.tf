7855010eb27550888bdfa1150d817c79da9e6b8c
jrojas-cloud <258112841+jrojas-cloud@users.noreply.github.com>
2026-06-18 12:24:11 -0600

    add static website config to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,34 @@ resource "random_id" "suffix" {
 resource "aws_s3_bucket" "static_site" {
   bucket = substr("${var.bucket_name}-${random_id.suffix.hex}", 0, 39)
 } 
+
+resource "aws_s3_bucket_website_configuration" "static_website_config" {
+  bucket = aws_s3_bucket.static_site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "static_site_policy" {
+  bucket = aws_s3_bucket.static_site.id
+  policy = jsonencode({
+    version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Actions = "s3:GetObject"
+        Resource = "${aws_s3_bucket.static_site.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_public_access_block" "static_site_access" {
+  bucket                  = aws_s3_bucket.static_site.id
+  block_public_acls       = false
+  block_public_policy     = false 
+  ignore_public_acls      = false 
+  restrict_public_buckets = false 
+}
