ea0a66b43b5d84769683480d119cd18570de1c8c
jrojas-cloud <258112841+jrojas-cloud@users.noreply.github.com>
2026-06-17 23:36:13 -0600

    fix aws region mismatch

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket         = "tf-resources-gha9"
-    region         = "us-east-1"
+    region         = "eu-central-1"
     key            = "github-actions/terraform.tfstate"
     encrypt        = true
     dynamodb_table = "if-reources-gha-lock9"
